8e8a003d5cab9e6d29d0c9706013a1f0578011eb
Isaac Lyne <IsaactimothyLyne@gmail.com>
2021-04-19 22:52:01 +1200

    Home style updates

--- Source/Scripts/main.js
+++ Source/Scripts/main.js
@@ -19,7 +19,9 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("myTopnav").style.backgroundColor = '#7FA8AE';
+    document.getElementById("myTopnav").style.borderBottom = '#000 solid';
   } else {
     document.getElementById("myTopnav").style.backgroundColor = '';
+    document.getElementById("myTopnav").style.borderBottom = '';
   }
 }
