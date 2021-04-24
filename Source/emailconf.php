8765849a0033c5cf3529764e33e61246611e2ebb
Isaac Lyne <IsaactimothyLyne@gmail.com>
2021-04-24 18:41:04 +1200

    Email Settings for customization

--- Source/emailconf.php
+++ Source/emailconf.php
@@ -1,19 +1,32 @@
 <?php
 if(isset($_POST['submitted'])){
+
+        ///////////////////////////////////
+        //Settings for your email address//
+        ///////////////////////////////////
+        
+        ini_set('SMTP','enlock-co-nz.mail.protection.outlook.com');//Email Server
+        $mailTo = 'isaacl@enlock.co.nz';//Email 
+        
+
+        //All variables from contact form
         $name = $_POST['firstname'];
         $mailfrom = $_POST['email'];
         $subject = $_POST['subject'];
         $message = $_POST['content'];
 
-        $mailTo = 'isaacl@enlock.co.nz';       
         $headers['From'] = $mailfrom;
 
+        //Formatting for the email to be sent
         $txt = "Hey! You have recieved an email from ".$name."\n\n".$message;
 
+        //settings for email port
+        ini_set('smtp_port',25);
+        
+        //send the email
         mail($mailTo, $subject, $txt, $headers);
-        echo '<script type="text/javascript">',
-        'emailsent();',
-        '</script>'
-        header("Location: index.html");
+        
+        //Redirect to contact page
+        header("Location: contact.html#mailSent");
 }
 ?>
