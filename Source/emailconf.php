04b4d3496f91f0d2b4b55d198ea9770868585787
Isaac Lyne <IsaactimothyLyne@gmail.com>
2021-04-24 14:25:49 +1200

    email finally sending
    Email sendin still working through the bounceback issue

--- Source/emailconf.php
+++ Source/emailconf.php
@@ -1,38 +1,15 @@
 <?php
-require_once 'System.php';
-if (class_exists('System')) {
- echo 'got Pear';
-} else {
- echo 'no Pear';
-};
-/*require_once 'C:\xampp\php\PEAR';
 if(isset($_POST['submitted'])){
-        //grab form values into variables
-        $name = $_POST['firstname']+$_POST['lastname'];
+        $name = $_POST['firstname'];
         $mailfrom = $_POST['email'];
         $subject = $_POST['subject'];
         $message = $_POST['content'];
 
-        //create transport
-        $transport = (new Swift_SmtpTransport('enlock-co-nz.mail.protection.outlook.com', 25))
-        ->setUsername('admin')
-        ->setPassword('swiftmailer1')
-        ;
-
-        //create the mailer using transport
-        $mailer = new Swift_Mailer($transport);
-
-        //custom formatting of variables
-        $mailTo = 'isaacl@enlock.co.nz';    
-        $mailToName = 'Isaac Lyne';   
+        $mailTo = 'isaacl@enlock.co.nz';       
         $headers = "From: ".$mailfrom;
+
         $txt = "Hey! You have recieved an email from ".$name."\n\n".$message;
 
-        $mailmessage = (new Swift_Message($subject))
-        ->setFrom([$mailfrom => $name])
-        ->setTo([$mailTo => $mailToName])
-        ->setBody($txt);
-
-        $result = $mailer->send($mailmessage);
-}*/
+        mail($mailTo, $subject, $txt, $headers);
+}
 ?>
